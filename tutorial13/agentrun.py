f419c03e7d41c09d3d8ba3198d7000d5f1f0b1fb
ronidas39 <ronidas041987@gmail.com>
2023-12-12 15:08:33 +0530

    tutorial14

--- tutorial13/agentrun.py
+++ tutorial13/agentrun.py
@@ -6,7 +6,7 @@ llm=ChatOpenAI(temperature=0,model="gpt-4")
 os.environ["SERPAPI_API_KEY"]="6bc26c990eab01a865f08b77ed174dbe4eb5a5b7f031b6a8ee621187228cf95e"
 tools=load_tools(["serpapi","llm-math"],llm=llm)
 
-agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,verbose=True)
+agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,verbose=True,return_intermediate_steps=True)
 with get_openai_callback() as cost:
     result=agent.run("who is the present prime minister of India,what is hios current age raise to the power of .5")
     print(f"total tokens: {cost.total_tokens}")
