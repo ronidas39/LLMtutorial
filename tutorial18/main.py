1c9cd2a015d198c999a074057704f5db4006a2a0
ronidas39 <ronidas041987@gmail.com>
2023-12-18 18:24:11 +0530

    tutorial19-20

--- tutorial18/main.py
+++ tutorial18/main.py
@@ -1,6 +1,5 @@
 from langchain.agents import AgentType,initialize_agent,load_tools
 from langchain.chat_models import ChatOpenAI
-
 llm=ChatOpenAI(temperature=0,model="gpt-4")
 tools=load_tools(["ddg-search"],llm=llm)
 agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,verbose=True)
