3a08d1a3b4a319ca1b274763e169cde778d6aa18
ronidas39 <ronidas041987@gmail.com>
2023-12-24 20:08:13 +0530

    tutorial21

--- tutorial18/main.py
+++ tutorial18/main.py
@@ -3,4 +3,4 @@ from langchain.chat_models import ChatOpenAI
 llm=ChatOpenAI(temperature=0,model="gpt-4")
 tools=load_tools(["ddg-search"],llm=llm)
 agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,verbose=True)
-agent.run("what is latest news on 1st odi bertween India and south africa")
+agent.run("top 5 business use case on LLM and generative ai")
