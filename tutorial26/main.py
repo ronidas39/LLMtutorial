ffe27be0e060d32c0aa95c1dfaf0429c2197b6c2
ronidas39 <ronidas041987@gmail.com>
2024-01-02 07:16:02 +0530

    t26

--- tutorial26/main.py
+++ tutorial26/main.py
@@ -1,3 +1,5 @@
 from langchain.document_loaders import JSONLoader
-
-loader=JSONLoader(file_path="sample.json",jq_schema=".")
+loader=JSONLoader(file_path="sample.json",jq_schema=".",json_lines=True,text_content=False)
+docs=loader.load()
+for doc in docs:
+    print(doc.page_content)
