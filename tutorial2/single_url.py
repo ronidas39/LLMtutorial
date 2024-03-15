d019120040cd9a06856a9676873af2e46177f996
ronidas39 <ronidas041987@gmail.com>
2024-03-15 23:57:01 +0530

    tutorial46

--- tutorial2/single_url.py
+++ tutorial2/single_url.py
@@ -1,7 +1,7 @@
 from langchain.document_loaders import youtube
 import io
 
-loader=youtube.YoutubeLoader.from_youtube_url("https://youtu.be/QcWtGoDLEz4")
+loader=youtube.YoutubeLoader.from_youtube_url("https://youtu.be/YNb-aF733ew")
 docs=loader.load()
 print(docs)
 with io.open("transcript.txt","w",encoding="utf-8")as f1:
