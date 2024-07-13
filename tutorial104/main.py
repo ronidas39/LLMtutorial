49fd3c607c2248e8cca7cc79c243381003769bc2
ronidas39 <ronidas041987@gmail.com>
2024-07-13 13:04:14 +0530

    tutorial104

--- tutorial104/main.py
+++ tutorial104/main.py
@@ -1,21 +1,29 @@
-import boto3
+
+#pip3 install boto3
+import boto3,json
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-
-client=boto3.resource("dynamodb",region_name="us-east-1")
-table=client.Table("token_logs")
-
+from datetime import datetime
+def update_table(item):
+    client=boto3.resource("dynamodb")
+    table=client.Table("token_logs")
+    table.put_item(Item=item)
 llm=ChatOpenAI(model="gpt-4o")
-template="""
-you are intelligent assistant who can write any article on {topic} with nice and attractive lines.
-output will only article nothinbg extra
+template="""you are intelligent assistant who can answer any {question} give by the user
+response must be a json only , with the following keys,nothing else:
+qsn:
+ans:
 """
 prompt=PromptTemplate.from_template(template)
 chain=prompt|llm
-response=chain.invoke({"topic":"Indian politics"})
+response=chain.invoke({"question":"write a story on king james"})
 answer=response.content
-response_id=response.id
-data=response.usage_metadata
-data.update({"response_id":response_id,"answer":response.content,"question":template})
-table.put_item(Item=data)
-
+answer=answer.replace("json","")
+answer=answer.replace("`","")
+answer=json.loads(answer)
+metadata=response.usage_metadata
+metadata["response_id"]=response.id
+metadata["qsn"]=answer["qsn"]
+metadata["ans"]=answer["ans"]
+metadata["timestamp"]=str(datetime.now())
+update_table(metadata)
