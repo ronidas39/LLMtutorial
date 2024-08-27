b2347acb32d7c6dd2af9560c57d2e18a9d8a0cd5
ronidas39 <49780975+ronidas39@users.noreply.github.com>
2024-08-27 15:14:44 +0530

    Update main.py

--- tutorial108/main.py
+++ tutorial108/main.py
@@ -1,7 +1,6 @@
 from streamlit_mic_recorder import speech_to_text
 import streamlit as st
 from emailAgent import runAgent
-
 st.title("your one stop voice enable email Assistant")
 st.write("instruct your voice and see the magic")
 text=speech_to_text(language="en",use_container_width=True,just_once=True,key="STT")
