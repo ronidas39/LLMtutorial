79fdaa3c8c6087aba236ca2a3805a2eca879b3bc
ronidas39 <ronidas041987@gmail.com>
2024-10-30 20:56:57 +0530

    tutorial117

--- tutorial117/main.py
+++ tutorial117/main.py
@@ -1,26 +1,23 @@
-from langchain_community.document_loaders import RedditPostsLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import ChatOpenAI,OpenAIEmbeddings
-from langchain_chroma import Chroma
-from langchain_community.vectorstores.utils import filter_complex_metadata
-from langchain.chains.question_answering import load_qa_chain
+import io
+from langchain_core.documents import Document
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 llm=ChatOpenAI(model="gpt-4o")
-loader=RedditPostsLoader(
-                        client_id="7aJt9CuSBwg4unyMjk8kZg", 
-                        client_secret="v1UUmCA4VHU-0MLzzR11pHYv7dRzXA", 
-                        user_agent="Sad_Mud_4484", 
-                        search_queries=["IndiaCricket"], 
-                        mode= "subreddit", 
-                        number_posts= 100
-                         )
-docs=loader.load()
-ts=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=20)
-sd=ts.split_documents(docs)
-vs=Chroma.from_documents(filter_complex_metadata(sd),embedding=OpenAIEmbeddings(),persist_directory="./vs")
-vs=Chroma(persist_directory="./vs",embedding_function=OpenAIEmbeddings())
-retriever=vs.as_retriever(k=5)
-match_docs=retriever.invoke("india vs newzeland test")
-print(match_docs)
-chain=load_qa_chain(llm=llm,chain_type="stuff")
-response=chain.run(input_documents=match_docs,question="how Washington Sundar performed?")
+import os,glob
+cwd=os.getcwd()
+files=glob.glob(cwd+"/*.txt")
+documents=[]
+for file in files:
+    with io.open(file,"r",encoding="utf-8")as f1:
+        data=f1.read()
+        f1.close()
+    documents.append(Document(page_content=data,metadata={"title":file.split("\\")[-1]}))
+# print(documents)
+
+prompt=ChatPromptTemplate.from_template("Summarize this content with little bullet :{context}")
+chain=create_stuff_documents_chain(llm,prompt)
+response=chain.invoke({"context":documents})
 print(response)
+    
+
--- tutorial2/single_url.py
+++ tutorial2/single_url.py
@@ -1,7 +1,8 @@
-from langchain.document_loaders import youtube
+#from langchain.document_loaders import youtube
+from langchain_community.document_loaders import YoutubeLoader
 import io
 
-loader=youtube.YoutubeLoader.from_youtube_url("https://youtu.be/7iEZ6OrKk6Q")
+loader=YoutubeLoader.from_youtube_url("https://youtu.be/bjHQZ27qe3g")
 docs=loader.load()
 print(docs)
 with io.open("transcript.txt","w",encoding="utf-8")as f1:
